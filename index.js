54a9ac36acc8f942f7de9680543616f56860128e
masontang@aol.com <masontang@aol.com>
2018-07-04 02:19:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-
+const katzDeliLine = []
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
-  for(var i = 0; i < katzDeliLine.length; i++)
-    var num = i + 1
-  return `Welcome, ${katzDeliLine}. You are number ${num} in line.`
+  var num = katzDeliLine.length
+  return `Welcome, ${name}. You are number ${num} in line.`
+}
+
+function nowServing (katzDeliLine){ 
+  var arr = []
+  if(katzDeliLine.length === 0 ){ 
+    arr.push('There is nobody waiting to be served!')
+    return arr
+  }else{ 
+    arr.push(`Currently serving ${katzDeliLine[0]}`)
+    katzDeliLine.shift()
+    return arr
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return 'The line is currently empty.'
+  }else {
+      var arr = []
+      for(var i = 0; i < katzDeliLine.length; i++){
+        var number = i + 1
+        arr.push(`${number}. ${katzDeliLine[i]}`)
+      }
+      return `The line is currently: ${arr.join(', ')} `
+  }
 }
